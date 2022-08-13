24c95d64122325f0c106a016fa687b3aa0933638
Leon <leon@youdevils.com>
2022-08-13 13:31:43 +1200

    Initial2

--- profileme/profileme/settings.py
+++ profileme/profileme/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5&18d!n$icwzbbl9pw(kr$$!@vp)u!=9lqc!wa@s)12g3n%s2&"
+SECRET_KEY = ""
+with open(os.path.join(BASE_DIR, "securitykey.txt")) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- profileme/website/views.py
+++ profileme/website/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import (
+    TemplateView,
+    # CreateView,
+    # FormView,
+    # ListView,
+    # UpdateView,
+    # RedirectView,
+)
+
+
+class Home(TemplateView):
+
+    # Set Template
+    template_name = "website/templates/index.html"
+
+    def get(self, request, *args, **kwargs):
+        # Process Year Selector Use | Update Dashboard Year
+
+        # Package Template Data
+        context = super().get_context_data(**kwargs)
+        # context.update() Add Custom Context Data
+        return render(request, self.template_name, context=context)
